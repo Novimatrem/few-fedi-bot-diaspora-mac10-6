ef832315bea608c146847e5ea7fa4d129e13dc0b
Novi <thenovimatrem@protonmail.ch>
2023-09-23 21:00:55 +0000

    put a helpful note in loop-forever-runme.sh

--- loop-forever-runme.sh
+++ loop-forever-runme.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# the script that runs
+# futa-every-word-bot-for-diaspora_macOS-10-6-snow-leopard-applescript
+
+# remember: set the mac to never go off/to sleep, never have it be interrupted
+# have firefox open, on SPECIFICALLY THIS PAGE: https://iviv.hu/status_messages/new , 
+# with the main posting box selected, cursor flashing.
+#
+# (ensure you make it be using the Mobile version of disapora, normal one no worky on old browser)
+# (it's set by a cookie so you only have to make it use mobile once, if the posting box shows up you good)
+#
+
 while true
 do
 sleep 1
